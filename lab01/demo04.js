ca2beba41e825c648226243fcacf190a6a26e663
Giang Le <wikihead99@gmail.com>
2022-01-10 23:44:11 +0700

    feature(lab01): ES - compare variable

---
+++ lab01/demo04.js
@@ -0,0 +1,13 @@
+/**
+ * so sanh kieu du lieu
+ */
+
+console.log('1 == 1 =>',1==1)
+console.log('1.0 == 1 =>',1.0==1)
+console.log("1 == '1' =>",1=='1')
+console.log("1.0 == '1'",1.0=='1')
+console.log("-----------------------------------")
+console.log("1 === 1 =>",1===1)
+console.log("1.0 === 1 =>",1.0===1)
+console.log("1 === '1' =>",1==='1')
+console.log("1.0 === '1' =>",1.0==='1')
