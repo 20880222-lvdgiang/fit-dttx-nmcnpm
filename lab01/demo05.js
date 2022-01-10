05bc456592d8d959afb682f4322633d0668ceec4
Giang Le <wikihead99@gmail.com>
2022-01-11 00:26:11 +0700

    refactor(lab01): ES - operator

--- lab01/demo05.js
+++ lab01/demo05.js
@@ -1,5 +1,5 @@
 /**
- * Các phép toán và phép so sánh
+ * Các phép toán
  */
 
 let num=100
@@ -13,9 +13,20 @@ console.log("num = num % 3 =>",num %3)
 console.log("num = num / 3 =>",parseInt(num /3))
 console.log("num = num / 3 =>",Math.floor(num/3))
 
+num=100
 console.log("num =",num)
 console.log("num++ =>",num++)
 console.log("num-- =>",num--)
 console.log("num =",num)
 console.log("++num =>",++num)
 console.log("--num =>",--num)
+
+num=100
+num+=5
+console.log("num += 5 =>",num)
+num-=4
+console.log("num -= 4 =>",num)
+num*=2
+console.log("num *= 2 =>",num)
+num/=6
+console.log("num /= 6 =>",num)
