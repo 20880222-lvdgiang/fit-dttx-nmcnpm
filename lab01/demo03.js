0a6a84428ede4e2266986b6ea61e6f558db94a55
Giang Le <wikihead99@gmail.com>
2022-01-10 23:29:26 +0700

    feature(lab01): ES - convert data-type

---
+++ lab01/demo03.js
@@ -0,0 +1,25 @@
+/**
+ * Chuyển đổi kiểu dữ lieju cho biến
+ */
+
+let number=100
+console.log("number =",number)
+
+number="Hello"
+console.log('number =',number)
+
+let b=1
+let c=2
+let d=b+c
+console.log(d)
+
+let e='1'
+let f='2'
+let g=e+f
+console.log(g)
+
+number=parseInt('123.45')
+console.log('number =',number)
+
+number=parseFloat('123.45')
+console.log('number =',number)
