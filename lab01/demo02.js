8bf7f78ee7485b25b5294dcc150c96edfc0d4271
Giang Le <wikihead99@gmail.com>
2022-01-10 23:10:32 +0700

    feature(lab01): ES - data type

---
+++ lab01/demo02.js
@@ -0,0 +1,31 @@
+/**
+ * ES - Kiểu dữ liệu
+ */
+
+let num =1
+let float_num=1.25
+
+console.log("Number -interger:",num)
+console.log("number - float:",float_num)
+
+let string ="Hello World"
+console.log("string:",string)
+
+let object={
+    id:1,
+    name:'product 01',
+    price:1000
+};
+
+console.log("object:",object)
+
+let array=[1,2,3,4,5]
+console.log("array:",array)
+
+let undefine_obj
+console.log("undefine",undefine_obj)
+
+let null_obj =100
+console.log("null_obj",null_obj)
+null_obj=null
+console.log("null_obj",null_obj)
